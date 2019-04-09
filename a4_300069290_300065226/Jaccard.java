499a5981ddc38bc09ba37fd9bb55bf3051e30b09
Soorya S <suri111200@gmail.com>
2019-04-08 22:45:25 -0400

    Complete final version

--- a4_300069290_300065226/Jaccard.java
+++ a4_300069290_300065226/Jaccard.java
@@ -1,6 +1,7 @@
+//import java.util.Arrays;
 public class Jaccard implements Similarity{
 	
-
+	/*
 	public double score (WordMap a, WordMap b){
 		
 		//AnB
@@ -22,5 +23,47 @@ public class Jaccard implements Similarity{
 		int denominator =theKeysA.length+theKeysB.length-numerator;
 
 		return numerator/denominator;
+	}
+	*/
+
+
+	public double score(WordMap a, WordMap b){
+		double toReturn;
+
+		String[] listOne=a.keys();
+		String[] listTwo=b.keys();
+
+		double numerator=0;
+		double denominator=0;
+
+		if(a==null || b==null){
+			toReturn=0;
+		}
+		if(a==null && b==null){
+			toReturn=1;
+		}
+		//n
+		for(int i=0; i<listTwo.length; i++){
+			if(a.contains(listTwo[i])){
+				numerator++;
+			}
+		}
+    	
+    	//u
+    	for(int i=0; i<listOne.length; i++){
+    			denominator++;
+    	}
+    	for(int i=0; i<listTwo.length; i++){
+    		if(a.contains(listTwo[i])==false){
+    			denominator++;
+    		}
+    	}
+
+    	if(denominator==0)
+    		toReturn=1;
+    	else
+    		toReturn=(numerator/denominator);
+    	
+		return toReturn;
 	}
 }
